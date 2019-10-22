a47f8f2ff958e736ed6bf3c8377a76d64af6a4fb
Alexander Shaidurov <shaa01@mail.ru>
2019-10-22 22:09:35 +0700

    Пересобрал структуру решения

---
+++ GameLogic/GameField.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class GameBoard
+    {
+        private TypeShape[,] fields;
+        private int countCellWidth;
+        private int countCellHeight;
+
+        public GameBoard(int _countCellWidth, int _countCellHeight)
+        {
+            countCellWidth = _countCellWidth;
+            countCellHeight = _countCellHeight;
+
+            fields = new TypeShape[countCellHeight, countCellWidth];
+            this.CellVisitor((x, y, shp) => fields[y, x] = TypeShape.Empty);
+        }
+
+        public void CellVisitor(CellVisitorHandler cellVisitorHandler)
+        {
+            for (int i = 0; i < countCellHeight; i++)
+                for (int j = 0; j < countCellWidth; j++)
+                    cellVisitorHandler(j, i, fields[i, j]);
+        }
+
+        public void PutShape(TypeShape shape, int x, int y)
+        {
+            if (x >= countCellWidth || y >= countCellHeight) return;
+            fields[y, x] = shape;
+        }
+    }
+}
---
+++ GameLogic/Types.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public enum TypeShape
+    {
+        Empty,
+        Circle,
+        Triangle,
+        Square
+    }
+
+    public delegate void CellVisitorHandler(int x, int y, TypeShape shape);
+}
