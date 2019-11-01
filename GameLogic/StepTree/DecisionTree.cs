0641e75cbdab048260dc5b7bafb61bf7d9d2882b
Alexander Shaidurov <shaa01@mail.ru>
2019-11-01 21:41:29 +0700

    объекты дерева

---
+++ GameLogic/StepTree/DecisionTree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    public class DecisionTree
+    {
+        Dictionary<byte[], List<ShapeLocation>> Positions;
+    }
+}
